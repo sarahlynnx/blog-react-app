da8e07c5b5a1a7e9f9f1d3e49c5ad49664ce3452
sarahlynnx <xsarahlynn06x@gmail.com>
2024-05-04 16:06:19 -0700

    Fixed images not showing on blog list

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import "./bloglist.css";
 import { getApiUrl } from "../api";
 import { formatDate } from "../DateFormat";
 import { UserContext } from "../userContext";
+import './bloglist.css';
 
 const BlogList = () => {
   const navigate = useNavigate();
   const [blogs, setBlogs] = useState([]);
   const { currentUser, logout } = useContext(UserContext);
 
-const handleLogoutSubmit = async () => {
+  const handleLogoutSubmit = async () => {
     logout();
-    localStorage.removeItem('token'); 
-    console.log('Logout successful!');
-    navigate('/');
-}
+    localStorage.removeItem("token");
+    console.log("Logout successful!");
+    navigate("/");
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -40,67 +40,59 @@ const handleLogoutSubmit = async () => {
   }, []);
 
   return (
-    <>
-      <Container>
-        <div className="blog-header">
-          <h2>All Posts</h2>
-          {currentUser ? (
-            <div>
-              <button
-                className="blog-login-btn"
-                onClick={() => handleLogoutSubmit()}
-              >
-                Logout
-              </button>
-              {currentUser.role === "author" && (
-                <button
-                  className="blog-login-btn"
-                  onClick={() => navigate("/blog/create")}
-                >
-                  New blog
-                </button>
-              )}
-            </div>
-          ) : (
-            <Link className="blog-login-link" to={"/login"}>
-              <button
-                className="blog-login-btn"
-              >
-                Login / Sign Up
-              </button>
-            </Link>
-          )}
-        </div>
-        <div className="blog-preview">
-          {blogs.map((blog) => (
-            <Link
-              className="ind-blog-link"
-              to={`/blog/${blog._id}`}
-              key={blog._id}
+    <Container>
+      <div className="blog-header">
+        <h2>All Posts</h2>
+        {currentUser ? (
+          <div>
+            <button
+              className="blog-login-btn"
+              onClick={() => handleLogoutSubmit()}
             >
-              <Row className="blog-row">
-                <Col sm className="p-2">
-                  <div className="blog-img">
+              Logout
+            </button>
+            {currentUser.role === "author" && (
+              <button
+                className="blog-login-btn"
+                onClick={() => navigate("/blog/create")}
+              >
+                New blog
+              </button>
+            )}
+          </div>
+        ) : (
+          <Link className="blog-login-link" to={"/login"}>
+            <button className="blog-login-btn">Login / Sign Up</button>
+          </Link>
+        )}
+      </div>
+      <div className="blog-preview">
+        {blogs.map((blog) => (
+          <Link className="ind-blog-link" to={`/blog/${blog._id}`} key={blog._id}>
+            <Row className="blog-row">
+              <Col sm className="p-2">
+                <div className="blog-img">
+                  {blog.images.length > 0 && (
                     <img
-                      src={blog.images[0]}
+                      src={`${getApiUrl("/api/posts/")}${blog._id}/image/${blog.images[0]._id}`}
                       alt={`Thumbnail for ${blog.title} Blog`}
                     />
-                  </div>
-                </Col>
-                <Col sm className="p-2">
-                  <div className="blog-info">
-                    <p className="blog-author-date">{blog.author.name}</p>
-                    <p className="blog-author-date">{formatDate(blog.date)}</p>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.content}</p>
-                  </div>
-                </Col>
-              </Row>
-            </Link>
-          ))}
-        </div>
-      </Container>
-    </>
+                  )}
+                </div>
+              </Col>
+              <Col sm className="p-2">
+                <div className="blog-info">
+                  <p className="blog-author-date">{blog.author.name}</p>
+                  <p className="blog-author-date">{formatDate(blog.date)}</p>
+                  <h2>{blog.title}</h2>
+                  <p>{blog.content}</p>
+                </div>
+              </Col>
+            </Row>
+          </Link>
+        ))}
+      </div>
+    </Container>
   );
 };
 
