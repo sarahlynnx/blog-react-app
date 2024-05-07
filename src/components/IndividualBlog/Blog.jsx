a2435b88830752a7d0e8a2be1db41b5285279fa8
sarahlynnx <xsarahlynn06x@gmail.com>
2024-05-07 13:38:28 -0700

    Added delete post request integration for authors

--- src/components/IndividualBlog/Blog.jsx
+++ src/components/IndividualBlog/Blog.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './blog.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import BlogContent from './BlogContent';
 import SharingIcons from './SharingIcons';
 import Interactions from './Interactions';
 import CommentsSection from './CommentsSection';
 import CommentForm from './CommentForm';
+import { UserContext } from "../userContext";
 import { getApiUrl, getBlogImageUrl } from '../api';
 import { formatDate } from '../DateFormat';
 
 const Blog = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
+    const { currentUser } = useContext(UserContext);
     const [comment, setComment] = useState('');
     const [blogData, setBlogData] = useState({
         title: '',
@@ -18,12 +21,13 @@ const Blog = () => {
         images: [],
         likes: 0,
         views: 0,
-        author: '',
+        author: {},
         date: '',
         comments: [],
         likedByUser: false
     });
 
+    // like post
     const handleLike = async () => {
         const token = localStorage.getItem('token');
         try {
@@ -41,6 +45,7 @@ const Blog = () => {
                     likes: data.likes,
                     likedByUser: data.likedByUser
                 }));
+                console.log(data.msg);
             } else {
                 throw new Error('Error updating likes');
             }
@@ -49,6 +54,7 @@ const Blog = () => {
         }
     };
 
+    // delete comment
     const handleDelete = async (commentId) => {
         const token = localStorage.getItem('token');
         try {
@@ -66,12 +72,42 @@ const Blog = () => {
                 ...prevData,
                 comments: prevData.comments.filter(comment => comment._id !== commentId)
             }));
+            const data = await response.json();
+            alert(data.msg);
         } catch (error) {
-            console.error('Error deleting comment:', error);
+            console.error(error);
             alert('An error occurred while deleting the comment. Please try again.');
         }
     };
 
+    // Delete post
+    const handleDeletePost = async () => {
+        const token = localStorage.getItem('token');
+        if (!window.confirm('Are you sure you want to delete this post?')) {
+            console.log('Deletion cancelled');
+            return;
+        };
+        try {
+            const response = await fetch(getApiUrl(`/api/posts/${id}`), {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Failed to delete post');
+            }
+            const data = await response.json();
+            alert(data.msg);
+            navigate("/");
+        } catch (error) {
+            console.error(error);
+            alert('An error occured while deleting the post. Please try again.');
+        }
+    };
+
+    // Edit comment
     const handleEdit = async (commentId, updatedContent) => {
         const token = localStorage.getItem('token');
         try {
@@ -98,6 +134,7 @@ const Blog = () => {
         }
     };
 
+    // Post comment
     const handleSubmit = async (e) => {
         e.preventDefault();
         const token = localStorage.getItem('token');
@@ -121,6 +158,7 @@ const Blog = () => {
         }
     };
 
+    // Fetch Post Data on mount
     useEffect(() => {
         const fetchBlogData = async () => {
             console.log(`Component mounted or id changed: ${id}`);
@@ -129,14 +167,13 @@ const Blog = () => {
                 const post = await fetchPost(id);
                 const comments = await fetchComments(id);
                 const images = await fetchImages(id, post.images);
-
                 setBlogData({
                     title: post.title,
                     content: post.content,
                     images: images,
                     likes: post.likes,
                     views: post.views,
-                    author: post.author.name,
+                    author: post.author,
                     date: post.date,
                     comments: comments,
                     likedByUser: post.likedBy ? post.likedBy.includes(localStorage.getItem('userId')) : false
@@ -149,6 +186,7 @@ const Blog = () => {
         fetchBlogData();
     }, [id]);
 
+    // Get Individual Post
     const fetchPost = async (postId) => {
         const response = await fetch(getApiUrl(`/api/posts/${postId}`), {
             method: 'GET',
@@ -162,6 +200,7 @@ const Blog = () => {
         return await response.json();
     };
 
+    // Get all comments for individual post
     const fetchComments = async (postId) => {
         const response = await fetch(getApiUrl(`/api/comments?postId=${postId}`), {
             method: 'GET',
@@ -175,6 +214,7 @@ const Blog = () => {
         return await response.json();
     };
 
+    // Get all images for individual post
     const fetchImages = async (postId, images) => {
         const imagesData = await Promise.all(images.map(async (image) => {
             try {
@@ -199,17 +239,19 @@ const Blog = () => {
 
     return (
         <div className='blog-container'>
-            <div className='d-flex flex-row justify-content-between align-items-center p-2'>
+            <div className='individual-blog-header'>
                 <div className='d-flex flex-row gap-4'>
-                    <div>{blogData.author}</div>
+                    <div>{blogData.author.name}</div>
                     <div>{formatDate(blogData.date)}</div>
                 </div>
-                <div>
-                    <button>Edit</button>
-                    <button onClick={() => handleDelete(comment._id)}>Delete</button>
-                </div>
+                {currentUser && blogData.author._id === currentUser.id && (
+                    <div>
+                        <button className='author-blog-btns'>Edit</button>
+                        <button className='author-blog-btns' onClick={handleDeletePost}>Delete</button>
+                    </div>
+                )}
             </div>
-            <BlogContent title={blogData.title} content={blogData.content} images={blogData.images} />
+            <BlogContent title={blogData.title} content={blogData.content} images={blogData.images}/>
             <SharingIcons />
             <Interactions views={blogData.views} likes={blogData.likes} onLike={handleLike} />
             <CommentsSection comments={blogData.comments} handleDelete={handleDelete} handleEdit={handleEdit} />
