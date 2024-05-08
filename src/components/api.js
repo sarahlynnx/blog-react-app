5414d7602ce1eb20e392d25e6380981624ce964d
sarahlynnx <xsarahlynn06x@gmail.com>
2024-05-07 21:25:47 -0700

    Reverted package.json back

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,5 @@
 const HOST = process.env.REACT_APP_SERVER_HOST || 'https://blog-server-8yo5.onrender.com';
-export const getApiUrl = (endpoint) => `http://${HOST}${endpoint}`;
+export const getApiUrl = (endpoint) => `https://${HOST}${endpoint}`;
 export const getBlogImageUrl = (postId, imageId) => {
     return `${getApiUrl(`/api/posts/${postId}/image/${imageId}`)}`;
 };
